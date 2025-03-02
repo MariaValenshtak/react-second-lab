c04a952fe51bc4047c98e33cb18f73fdb73d59cf
MariaValenshtak <valenshtakm@gmai.com>
2025-03-02 20:10:38 +0200

    Done

---
+++ react-second-lab/src/data.js
@@ -0,0 +1,33 @@
+export const cardsData = [
+    {
+      id: 1,
+      title: "Лісова прогулянка",
+      description: "Затишний лісовий пейзаж для відпочинку на природі.",
+      image: "https://i.pinimg.com/736x/47/04/62/470462a93d8e066a35981f007fc7669b.jpg"
+    },
+    {
+      id: 2,
+      title: "Місто вночі",
+      description: "Нічне місто з яскравими вогнями та неймовірною атмосферою.",
+      image: "https://i.pinimg.com/736x/0f/fb/4b/0ffb4b651ea1c4738cb68191a791cf77.jpg"
+    },
+    {
+      id: 3,
+      title: "Гірський пейзаж",
+      description: "Краєвид величних гір під яскравим сонячним небом.",
+      image: "https://i.pinimg.com/736x/05/49/38/054938c718ccf43f026d6856d2ebc067.jpg"
+    },
+    {
+      id: 4,
+      title: "Морський берег",
+      description: "Спокійний пляж із чистою водою та м'яким піском.",
+      image: "https://i.pinimg.com/736x/4e/c5/15/4ec515da20f3b87d501013b671c5088f.jpg"
+    },
+    {
+      id: 5,
+      title: "Захід сонця",
+      description: "Романтичний захід сонця, що фарбує небо у дивовижні кольори.",
+      image: "https://i.pinimg.com/736x/62/6c/e7/626ce7034b327416bc430765b4a2a42b.jpg"
+    }
+  ];
+  
